87e15a9e363658a2eadcf751feecca99127cb216
petahans <143744745+petahans@users.noreply.github.com>
2023-08-31 19:54:27 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @brief		Public bootstrap
+ * @brief		API bootstrap
  * @author		<a href='http://www.invisionpower.com'>Invision Power Services, Inc.</a>
  * @copyright	(c) 2001 - 2016 Invision Power Services, Inc.
  * @license		http://www.invisionpower.com/legal/standards/
@@ -8,10 +8,6 @@
  * @since		18 Feb 2013
  * @version		SVN_VERSION_NUMBER
  */
- /*if($_SERVER['REMOTE_ADDR'] != '94.71.49.176'){
-echo "Temporary unscheduled maintenance<br> ETA: 5 minutes";
-exit;
-}*/
-$_SERVER['SCRIPT_FILENAME']	= __FILE__;
-require_once 'init.php';
-\IPS\Dispatcher\Front::i()->run();
+
+require_once '../init.php';
+\IPS\Dispatcher\Api::i()->run();
